78b1f13b53601218a3d580e70f297f6e7c088caa
fishming <1661342068@qq.com>
2020-11-09 17:08:34 +0800

    Applied new API to upper alg

--- include/Maze.h
+++ include/Maze.h
@@ -6,36 +6,42 @@
 #include <queue>
 #include <stack>
 #include <algorithm>
+#include "information.h"
 #define MAZE_SIZE 6
+#define OBSATCLE 8
 
 // image axis
 //            Start
 //             | |
-// -0--1--2--3--4--5-
-//  |  |  |  |  |  |
-// -6--7--8--9-10-11-
-//  |  |  |  |  |  |
-// -12-13-14-15-16-17-
-//  |  |  |  |  |  |
-// -18-19-20-21-22-23-
-//  |  |  |  |  |  |
-// -24-25-26-27-28-29-
-//  |  |  |  |  |  |
-// -30-31-32-33-34-35-
+// -1--2--3--4--5--6-
+//  |  |  |  |  |  |  
+// -7--8--9-10-11--12-
+//  |  |  |  |  |  |  
+// -13-14-15-16-17-18-
+//  |  |  |  |  |  |  
+// -19-20-21-22-23-24
+//  |  |  |  |  |  |  
+// -25-26-27-28-29-30
+//  |  |  |  |  |  |  
+// -31-32-33-34-35-36
 //    | |
 //    End
+
+struct bfsInfo
+{
+    int nextNode;
+    int dist;
+};
+
 class Maze {
 private:
     static std::map <int, std::list<int>> adjList;
-    static int Stack[MAZE_SIZE * MAZE_SIZE];
-    static std::vector<int> history;
-    static std::vector<int> Rotate;
 
 public:
-    static void initialize();
-    static void addEdge(int, int, bool);
-    static void genRoute();
-    static void bfs(int, int);
+    static void initialize(Information &info);
+    static void addEdge(int u, int v, bool dir);
+    static bfsInfo getDistance(int now, int target);
+    static int getDirection(int last, int now, int target);
 };
 
 #endif //maze_H
