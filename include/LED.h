414b184580f30e830741f711c4540e10735f7d63
fishming <1661342068@qq.com>
2020-11-11 14:06:31 +0800

    add LED

---
+++ include/LED.h
@@ -0,0 +1,18 @@
+#ifndef LED_H
+#define LED_H
+
+#include <Arduino.h>
+#define portLED 3
+
+class LED
+{
+private:
+    static uint8_t ledReg;
+public:
+    static void initialize();
+    static void ledOn();
+    static void ledOff();
+    static uint8_t ledStatus();
+};
+
+#endif //LED_H
