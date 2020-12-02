ca30851e31e80fdde491a9a7027b20e26e13a846
bowmanchow <bowmanchow@gmail.com>
2020-12-03 00:10:10 +0800

    fix Motor bug

--- include/statemachine.h
+++ include/statemachine.h
@@ -7,14 +7,14 @@
 
 // 各种Define，用于debug
 // #define USE_ZIGBEE
-#define DEBUG_MOTOR
+// #define DEBUG_MOTOR
 // #define DEBUG_ANGLECONTROLER
 // #define DEBUG_IRRECEIVER
 // #define DEBUG_ZIGBEE
 // #define DEBUG_TIMER
-#define DEBUG_POSITION
-#define DEBUG_MAZE_POS
-#define DEBUG_CROSS_ACTION
+// #define DEBUG_POSITION
+// #define DEBUG_MAZE_POS
+// #define DEBUG_CROSS_ACTION
 
 class StateMachine // The statemachine of th2e car (Singleton)
 {
